730d5b82e3493181ff67e8563986dc9a6f8f6752
kiencang <78894062+kiencang@users.noreply.github.com>
2025-12-03 13:28:54 +0700

    Add files via upload

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ so_luong=10
 # Cấu trúc: if [ điều_kiện ]; then ... fi
 # Lưu ý: BẮT BUỘC phải có khoảng trắng sau [ và trước ]
 
-if [ $so_luong -gt 5 ]; then
+if [$so_luong -gt 5]; then
     echo "Số lượng lớn hơn 5."
 else
     echo "Số lượng nhỏ hơn hoặc bằng 5."
