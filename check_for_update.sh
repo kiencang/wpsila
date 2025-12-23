9846ed847a545ed475b04bf7cbe8da3da99ec6ea
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2025-12-23 13:19:14 +0700

    Lên phiên bản 0.2.2

--- check_for_update.sh
+++ check_for_update.sh
@@ -2,7 +2,7 @@
 
 # -------------------------------------------------------------------------
 # wpsila - Update Manager
-# Version 0.2.1 - Optimized for Ubuntu 22.04 & 24.04
+# Version 0.2.2 - Optimized for Ubuntu 22.04 & 24.04
 # -------------------------------------------------------------------------
 
 set -euo pipefail
@@ -15,7 +15,7 @@ if [[ $EUID -ne 0 ]]; then
    exit $?
 fi
 
-SCRIPT_VERSION="v0.2.1"
+SCRIPT_VERSION="v0.2.2"
 GITHUB_USER="kiencang"
 GITHUB_REPO="wpsila"
 
