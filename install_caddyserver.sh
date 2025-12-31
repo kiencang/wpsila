08ceb7d4617f89a384831c31900a45bbfaae3391
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2025-12-31 19:11:07 +0700

    Bổ sung thêm dải IP của Cloudflare

--- install_caddyserver.sh
+++ install_caddyserver.sh
@@ -131,6 +131,11 @@ cat > /etc/caddy/Caddyfile <<EOF
     # Global Options
     # Tu dong quan ly SSL qua Email nay
     email $ADMIN_EMAIL
+	
+	# Cấu hình Trusted Proxies cho Cloudflare
+	servers {
+		trusted_proxies static private_ranges 173.245.48.0/20 103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 141.101.64.0/18 108.162.192.0/18 190.93.240.0/20 188.114.96.0/20 197.234.240.0/22 198.41.128.0/17 162.158.0.0/15 104.16.0.0/13 104.24.0.0/14 172.64.0.0/13 131.0.72.0/22 2400:cb00::/32 2606:4700::/32 2803:f800::/32 2405:b500::/32 2405:8100::/32 2a06:98c0::/29 2c0f:f248::/32
+	}	
 }
 
 # [QUAN TRONG] Import tat ca file cau hinh trong thu muc sites-enabled
