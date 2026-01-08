0f9a626601016334714407f534242a32f0dfddf7
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-01-08 10:45:09 +0700

    Thêm thông báo lệnh cần dùng cho đảm bảo

--- install_wpsila.sh
+++ install_wpsila.sh
@@ -369,7 +369,7 @@ ln -sf "$INSTALL_DIR/wpsila_menu.sh" "$BIN_LINK"
 if [[ -x "$BIN_LINK" ]]; then
     echo -e "${GREEN}=== CAI DAT THANH CONG! ===${NC}"
 	echo -e "Phien ban: ${GREEN}${SILA_VERSION}${NC}"
-    echo -e "Xin chuc mung ban! Hay go lenh: ${YELLOW}wpsila${NC} de bat dau su dung."
+    echo -e "Xin chuc mung ban! Hay go lenh: ${YELLOW}sudo wpsila${NC} de bat dau su dung."
 else
     error_exit "Loi khi tao lenh shortcut wpsila."
 fi
